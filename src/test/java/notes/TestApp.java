6b59e6db0f39626f18ad357b95a69cd052598c38
Felix Benz-Baldas <f.benz-baldas@mailbox.org>
2018-10-21 16:37:35 +0200

    add unused code

--- src/test/java/notes/TestApp.java
+++ src/test/java/notes/TestApp.java
@@ -2,8 +2,10 @@ package notes;
 
 import static org.junit.Assert.assertArrayEquals;
 
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.util.LinkedList;
 import java.util.List;
@@ -13,7 +15,7 @@ import org.junit.Test;
 public class TestApp {
 
 	@Test
-	public void testCreateNote() throws IOException, ClassNotFoundException {
+	public void testCreateNote() throws Exception {
 		createNote("Test-Notiz");
 		assertArrayEquals(new String[] { "Test-Notiz" }, getAllNotes());
 	}
@@ -23,25 +25,23 @@ public class TestApp {
 	public void createNote(String note) throws IOException {
 		data.add(note);
 
-		FileOutputStream fout = new FileOutputStream("testFile.ser");
-		ObjectOutputStream oos = new ObjectOutputStream(fout);
-		oos.writeObject(note);
-		oos.close();
 	}
 
 	public String[] getAllNotes() throws IOException, ClassNotFoundException {
 		return new String[] { data.get(0) };
-//		FileInputStream fin = new FileInputStream("testFile.ser");
-//		ObjectInputStream ois = new ObjectInputStream(fin);
-//		return new String[] { (String) ois.readObject() };
 	}
 	
-	public void save() {
-		
+	public void save() throws Exception{
+		FileOutputStream fout = new FileOutputStream("testFile.ser");
+		ObjectOutputStream oos = new ObjectOutputStream(fout);
+		oos.writeObject(data);
+		oos.close();
 	}
 	
-	public void load() {
-		
+	public void load() throws Exception {
+		FileInputStream fin = new FileInputStream("testFile.ser");
+		ObjectInputStream ois = new ObjectInputStream(fin);
+		data = (List<String>) ois.readObject();
 	}
 	
 }
