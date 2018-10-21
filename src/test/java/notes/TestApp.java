9456a89530ac8d6772896af92c6d8e958cc7992d
Felix Benz-Baldas <f.benz-baldas@mailbox.org>
2018-10-21 16:27:03 +0200

    create file

--- src/test/java/notes/TestApp.java
+++ src/test/java/notes/TestApp.java
@@ -2,17 +2,25 @@ package notes;
 
 import static org.junit.Assert.assertArrayEquals;
 
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+
 import org.junit.Test;
 
 public class TestApp {
 
 	@Test
-	public void testCreateNote() {
+	public void testCreateNote() throws IOException {
 		createNote("Test-Notiz");
 		assertArrayEquals(new String[] { "Test-Notiz" }, getAllNotes());
 	}
 
-	public void createNote(String note) {
+	public void createNote(String note) throws IOException {
+		FileOutputStream fout = new FileOutputStream("testFile.ser");
+		ObjectOutputStream oos = new ObjectOutputStream(fout);
+		oos.writeObject(note);
 	}
 
 	public String[] getAllNotes() {
