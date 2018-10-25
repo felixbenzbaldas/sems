b5ce937fb635ec36675b037be7982aac1838f4b1
Felix Benz-Baldas <f.benz-baldas@mailbox.org>
2018-10-25 21:18:49 +0200

    extract a class

--- src/main/java/notes/App.java
+++ src/main/java/notes/App.java
@@ -1,5 +1,44 @@
 package notes;
 
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.util.LinkedList;
+import java.util.List;
+
 public class App {
 
+	private List<String> data = new LinkedList<String>();
+
+	public void createNote(String note) throws IOException {
+		data.add(note);
+	}
+
+	public List<String> getAllNotes() throws IOException, ClassNotFoundException {
+		return data;
+	}
+
+	public void save(String path) throws Exception{
+		FileOutputStream fout = new FileOutputStream(path);
+		ObjectOutputStream oos = new ObjectOutputStream(fout);
+		oos.writeObject(data);
+		oos.close();
+	}
+	
+	public void save() throws Exception{
+		save("testFile.ser");
+	}
+
+	public void load(String path) throws Exception {
+		FileInputStream fin = new FileInputStream(path);
+		ObjectInputStream ois = new ObjectInputStream(fin);
+		data = (List<String>) ois.readObject();
+		ois.close();
+	}
+
+	public void load() throws Exception {
+		load("testFile.ser");
+	}
 }
--- src/test/java/notes/TestApp.java
+++ src/test/java/notes/TestApp.java
@@ -4,13 +4,6 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
-import java.util.LinkedList;
-import java.util.List;
 
 import org.junit.Test;
 
@@ -18,61 +11,27 @@ public class TestApp {
 
 	@Test
 	public void testCreateNote() throws Exception {
-		TestApp app = new TestApp();
+		App app = new App();
 		app.createNote("Test-Notiz");
 		assertEquals("Test-Notiz", app.getAllNotes().get(0));
 	}
 	
-
 	@Test
 	public void testSave() throws Exception {
 		String path = "src/test/resources/testData/testFile-" + Math.random() + ".ser";
-		new TestApp().save(path);
+		new App().save(path);
 		assertTrue(new File(path).exists());
 	}
 	
 	@Test
 	public void testLoad() throws Exception {
 		String path = "src/test/resources/testData/testFile-" + Math.random() + ".ser";
-		TestApp app1 = new TestApp();
+		App app1 = new App();
 		app1.createNote("test");
 		app1.save(path);
 		//
-		TestApp app2 = new TestApp();
+		App app2 = new App();
 		app2.load(path);
 		assertEquals("test", app2.getAllNotes().get(0));
 	}
-	
-	private List<String> data = new LinkedList<String>();
-
-	public void createNote(String note) throws IOException {
-		data.add(note);
-	}
-
-	public List<String> getAllNotes() throws IOException, ClassNotFoundException {
-		return data;
-	}
-
-	public void save(String path) throws Exception{
-		FileOutputStream fout = new FileOutputStream(path);
-		ObjectOutputStream oos = new ObjectOutputStream(fout);
-		oos.writeObject(data);
-		oos.close();
-	}
-	
-	public void save() throws Exception{
-		save("testFile.ser");
-	}
-
-	public void load(String path) throws Exception {
-		FileInputStream fin = new FileInputStream(path);
-		ObjectInputStream ois = new ObjectInputStream(fin);
-		data = (List<String>) ois.readObject();
-		ois.close();
-	}
-
-	public void load() throws Exception {
-		load("testFile.ser");
-	}
-	
 }
